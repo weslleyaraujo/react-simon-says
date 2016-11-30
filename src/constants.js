46f4f4ef7932989f72acc0f0d4db66b9e9dcaa89
Weslley Araujo <weslleyaraujo20@gmail.com>
2016-11-30 11:29:52 -0200

    refactor(constants): comma

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const colors = {
-  red: "#FF4136",
-  blue: "#0074D9",
-  green: "#2ECC40",
-  yellow: "#FFDC00",
+  red: '#FF4136',
+  blue: '#0074D9',
+  green: '#2ECC40',
+  yellow: '#FFDC00',
   white: 'white',
 }
