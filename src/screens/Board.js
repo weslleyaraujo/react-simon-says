2e2772fc9bbda4df3c70b236f143ca3fdf4db4c4
Weslley Araujo <weslleyaraujo20@gmail.com>
2016-12-04 10:40:05 -0200

    feat(*): bind keys + game-over styles

--- src/screens/Board.js
+++ src/screens/Board.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Flex } from 'reflexbox';
+import styled from 'styled-components';
+import { Flex, withReflex } from 'reflexbox';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { bind as bindKey } from 'mousetrap';
 
 import Block from '../components/Block';
 import Shell from '../components/Shell';
@@ -28,10 +30,51 @@ const Blocks = {
   ),
 }
 
+const AbsoluteCenter = withReflex()(styled.div`
+  position: absolute;
+  margin: auto;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 4;
+  text-align: center;
+  background-color: rgba(0,0,0,0.9);
+  height: ${props => props.size}px;
+`);
+
+const GrayScale = styled.div`
+  transition: 0.7s;
+  ${props => !props.active ? `` : `
+    filter: grayscale(100%);
+  `}
+`;
+
 class Board extends Component {
 
   componentDidMount() {
     this.startMatch();
+    const { actions } = this.props;
+    bindKey(['1', 'd'], () =>
+      actions.guessColor(this.getGuessPayload({ id: 'green' })));
+
+    bindKey(['2', 'f'], () =>
+      actions.guessColor(this.getGuessPayload({ id: 'red' })));
+
+    bindKey(['3', 'j'], () =>
+      actions.guessColor(this.getGuessPayload({ id: 'yellow' })));
+
+    bindKey(['4', 'l'], () =>
+      actions.guessColor(this.getGuessPayload({ id: 'blue' })));
+
+    bindKey(['space'], this.onSpacePress.bind(this));
+  }
+
+  onSpacePress() {
+    const { gameOver } = this.props.game;
+    if(gameOver) {
+      this.startMatch();
+    }
   }
 
   startMatch() {
@@ -53,15 +96,19 @@ class Board extends Component {
     );
   }
 
-  onBlockClick({ id }) {
-    const { match, actions } = this.props;
+  getGuessPayload({ id }) {
+    const { match } = this.props;
     const tail = match.guessed.length;
     const succeeded = match.all[tail] === id;
-
-    actions.guessColor({
+    return {
       id,
       succeeded,
-    });
+    }
+  }
+
+  onBlockClick({ id }) {
+    const { actions } = this.props;
+    actions.guessColor(this.getGuessPayload({ id }));
   }
 
   renderRow({ from, to }) {
@@ -79,26 +126,27 @@ class Board extends Component {
 
   render() {
     const { presentation, score, gameOver, highscore } = this.props.game;
-
     return (
       <Shell>
         <AbsoluteOnTop p={2} flex>
           <div style={{ color: 'white' }}>
             score: {score} <br />
             high score: {highscore} <br />
-            {gameOver && (
-              <div>
-                <p>game over</p>
-                <Button onClick={this.startMatch.bind(this)}>Try again</Button>
-              </div>
-            )}
           </div>
         </AbsoluteOnTop>
+        {gameOver &&
+          <AbsoluteCenter size={300} p={2}>
+            <h3>game over</h3>
+            <Button onClick={this.startMatch.bind(this)}>Try again</Button>
+          </AbsoluteCenter>
+        }
         <Player />
-        <span style={{ pointerEvents: (presentation || gameOver) ? 'none' : 'initial' }}>
-          {this.renderRow({ from: 0, to: 2 })}
-          {this.renderRow({ from: 2, to: 4 })}
-        </span>
+        <GrayScale active={gameOver}>
+          <span style={{ pointerEvents: (presentation || gameOver) ? 'none' : 'initial' }}>
+            {this.renderRow({ from: 0, to: 2 })}
+            {this.renderRow({ from: 2, to: 4 })}
+          </span>
+        </GrayScale>
       </Shell>
     );
   }
