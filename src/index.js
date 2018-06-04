67ec123d3719b1addf54b353386cc7d12d530c5e
Weslley Araujo <weslley.araujo@booking.com>
2018-06-04 18:26:08 +0200

    feat(*): adds manifest on index.html file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Route, Router, hashHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-
-import Welcome from './screens/Welcome';
-import Board from './screens/Board';
-import store from './store';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Route, Router, hashHistory } from "react-router";
+import { syncHistoryWithStore } from "react-router-redux";
+import registerServiceWorker from "./registerServiceWorker";
+import Welcome from "./screens/Welcome";
+import Board from "./screens/Board";
+import store from "./store";
+import "./index.css";
 
 const history = syncHistoryWithStore(hashHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history} >
-      <Route path="/" component={Welcome}  />
+    <Router history={history}>
+      <Route path="/" component={Welcome} />
       <Route path="/board/" component={Board} />
     </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+registerServiceWorker();
