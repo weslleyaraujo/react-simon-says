b81c99c57082f5c65d6bb08ed83007fea911a386
Weslley Araujo <weslley.araujo@booking.com>
2020-08-02 12:00:47 +0200

    Updates

---
+++ precache-manifest.68301201e6549cf446552f770d1745d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad15cb81323b5d78a5f88ff9d3b02e4",
+    "url": "/react-simon-says/index.html"
+  },
+  {
+    "revision": "f8b6329a81307a343e0c",
+    "url": "/react-simon-says/static/css/main.d93ef70b.chunk.css"
+  },
+  {
+    "revision": "5d482773f33bcbd9b0d9",
+    "url": "/react-simon-says/static/js/2.d3357f85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-simon-says/static/js/2.d3357f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b6329a81307a343e0c",
+    "url": "/react-simon-says/static/js/main.f192868e.chunk.js"
+  },
+  {
+    "revision": "7f8af093c2a9d47270b3",
+    "url": "/react-simon-says/static/js/runtime-main.d94022b1.js"
+  },
+  {
+    "revision": "d6f8a4c1c4fce8d9ad93c0bd33b478b7",
+    "url": "/react-simon-says/static/media/blue.d6f8a4c1.mp3"
+  },
+  {
+    "revision": "3c8ca9470540bbbea8eaa187bfe82a39",
+    "url": "/react-simon-says/static/media/green.3c8ca947.mp3"
+  },
+  {
+    "revision": "8d481077276f07445e615252af9a8be4",
+    "url": "/react-simon-says/static/media/red.8d481077.mp3"
+  },
+  {
+    "revision": "3ee582459fb09df5b041ef1b9593d7b5",
+    "url": "/react-simon-says/static/media/yellow.3ee58245.mp3"
+  }
+]);
